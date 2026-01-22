d731736d4139f7a65ef3cf18ef61b71d4a3d5277
israelcruzz <israelcruz.contato@gmail.com>
2026-01-22 00:44:32 -0300

    feat: add all routes to auth

--- src/Domains/Auth/Controllers/RefreshTokenController.ts
+++ src/Domains/Auth/Controllers/RefreshTokenController.ts
@@ -43,3 +43,5 @@ class RefreshTokenController implements IController {
         }
     }
 }
+
+export default new RefreshTokenController()
--- src/Domains/Auth/routes.ts
+++ src/Domains/Auth/routes.ts
@@ -1,9 +1,20 @@
 import express from "express"
-import SignInController from "./Controllers/SignInController"
+import SignInController from "@/Domains/Auth/Controllers/SignInController"
+import SignUpController from "@/Domains/Auth/Controllers/SignUpController"
+import ConfirmSignUpController from "@/Domains/Auth/Controllers/ConfirmSignUpController"
+import ResendConfirmationCodeController from "@/Domains/Auth/Controllers/ResendConfirmationCodeController"
+import ForgotPasswordController from "@/Domains/Auth/Controllers/ForgotPasswordController"
+import ResetPasswordController from "@/Domains/Auth/Controllers/ResetPasswordController"
+import RefreshTokenController from "@/Domains/Auth/Controllers/RefreshTokenController"
 
 const router = express.Router()
 
-router.post("/login", SignInController.handler)
-router.get("/signup", SignInController.handler)
+router.post("/signin", SignInController.handler)
+router.post("/signup", SignUpController.handler)
+router.post("/confirm-signup", ConfirmSignUpController.handler)
+router.post("/resend-confirmation-code", ResendConfirmationCodeController.handler)
+router.post("/forgot-password", ForgotPasswordController.handler)
+router.post("/reset-password", ResetPasswordController.handler)
+router.post("/refresh-token", RefreshTokenController.handler)
 
 export default router
